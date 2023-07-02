a057785cde41b1d680bcdd707531820422a5590c
SaberShip <{ID}+{username}@users.noreply.github.com>
2023-07-01 21:59:13 -0500

    revert init tweaks

--- custom_components/govee-ble-lights/__init__.py
+++ custom_components/govee-ble-lights/__init__.py
@@ -1,4 +1,4 @@
-`from __future__ import annotations
+from __future__ import annotations
 
 from homeassistant.components import bluetooth
 from homeassistant.config_entries import ConfigEntry
@@ -35,7 +35,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             )
 
     hass.data[DOMAIN][entry.entry_id] = Hub(hass, address, entry.data.get("type"))
-    await hass.config_entries.async_forward_entry_setup(entry, PLATFORMS)
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, PLATFORMS)
+    )
     return True
 
 
