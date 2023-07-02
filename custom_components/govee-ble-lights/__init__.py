38a2dc9c4a6aeaeee1c5a993e8b99a163f0a2f4d
SaberShip <{ID}+{username}@users.noreply.github.com>
2023-07-01 20:15:39 -0500

    entry setup impromvements

--- custom_components/govee-ble-lights/__init__.py
+++ custom_components/govee-ble-lights/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from homeassistant.components import bluetooth
+from homeassistant.components.bluetooth import async_scanner_count
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
@@ -22,11 +23,18 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     assert address is not None
     ble_device = bluetooth.async_ble_device_from_address(hass, address.upper(), True)
     if not ble_device:
+        if async_scanner_count(hass, connectable=False):
+            raise ConfigEntryNotReady(
+                "No bluetooth scanner detected. \
+                Enable the bluetooth integration or ensure an esphome device \
+                is running as a bluetooth proxy"
+            )
         raise ConfigEntryNotReady(
             f"Could not find LED BLE device with address {address}"
         )
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = Hub(hass, address, entry.data.get("type"))
+    hass.data[DOMAIN][entry.entry_id] = ble_device
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, PLATFORMS)
     )
