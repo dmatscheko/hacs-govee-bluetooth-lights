ddd573a15f9d3fbd8cb2d2885fefd366f96ea612
SaberShip <{ID}+{username}@users.noreply.github.com>
2023-07-01 20:40:07 -0500

    tweak entry setup

--- custom_components/govee-ble-lights/__init__.py
+++ custom_components/govee-ble-lights/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from homeassistant.components import bluetooth
-from homeassistant.components.bluetooth import async_scanner_count
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
@@ -19,11 +18,12 @@ class Hub:
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Govee BLE device from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
     address = entry.unique_id
     assert address is not None
     ble_device = bluetooth.async_ble_device_from_address(hass, address.upper(), True)
     if not ble_device:
-        if async_scanner_count(hass, connectable=False):
+        if bluetooth.async_scanner_count(hass, connectable=False):
             raise ConfigEntryNotReady(
                 "No bluetooth scanner detected. \
                 Enable the bluetooth integration or ensure an esphome device \
@@ -33,8 +33,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             f"Could not find LED BLE device with address {address}"
         )
 
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = Hub(hass, address, entry.data.get("type"))
-    hass.data[DOMAIN][entry.entry_id] = ble_device
+    hass.data[DOMAIN][entry.entry_id] = Hub(hass, address, entry.data.get("type"))
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, PLATFORMS)
     )
@@ -43,7 +42,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
 
